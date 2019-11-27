4cd80030f2e1f228457cc0daa27a727a60f7780a
muscleman <musclesonvacation@hotmail.com>
2019-11-27 03:32:43 -0600

    Better handle developer donations
    - Fix setting 0% donation
    - Fix/Improve logging for donation

--- init.js
+++ init.js
@@ -57,6 +57,10 @@ if (cluster.isWorker){
     return;
 }
 
+// Developer donations
+if (devFee < 0.2)
+       log('info', logSystem, 'Developer donation \(devDonation\) is set to %d\%, Please consider raising it to 0.2\% or higher !!!', [devFee]);
+
 // Initialize log system
 var logSystem = 'master';
 require('./lib/exceptionWriter.js')(logSystem);
