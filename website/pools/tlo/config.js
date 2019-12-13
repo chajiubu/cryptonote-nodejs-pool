3eba647288ef8bbeb8077822e161309a7eeda367
Corey Moyer <cronmod.dev@gmail.com>
2019-12-13 05:20:25 -0500

    Add Talleo pool

---
+++ website/pools/tlo/config.js
@@ -0,0 +1,17 @@
+var api = "https://funwithcrypto.tk:8127";
+let parentCoin = "talleo";
+
+//var email = "";
+var telegram = "https://t.me/FunWithCrypto";
+var discord = "https://discord.gg/nRPWrTB";
+//var facebook = "";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-LTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "https://pool.talleo.org/?hash={id}#blockchain_block";
+//var blockchainExplorerMerged = "";
+var transactionExplorer = "https://pool.talleo.org/?hash={id}#blockchain_transaction";
+//var transactionExplorerMerged = "";
+
+var themeCss = "/pools/common/themes/tlo.css";
+var defaultLang = 'en';
